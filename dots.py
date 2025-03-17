8a18b1cb8ca29d5ea4fe51d5b95ecf083e664a82
srijrao <srijc.rao@gmail.com>
2025-03-16 19:25:51 -0500

    Add Dot class and update settings for dot properties

---
+++ dots.py
@@ -0,0 +1,25 @@
+import settings
+
+class Dot:
+    def __init__(self, position=[0, 0], type="to food"):
+        self.position = position
+        self.type = type
+        self.setup()
+
+    def setup(self):
+        """ setup function for the dot """
+        self.size = settings.dot_size
+        self.timeleft = settings.dot_time
+        self.active = True
+        if self.type == "to food":
+            self.color = settings.GREEN
+        elif self.type == "to home":
+            self.color = settings.RED
+        else:
+            self.color = settings.WHITE
+
+    def update(self):
+        """ update function for the dot """
+        self.timeleft -= 1
+        if self.timeleft <= 0:
+            self.active = False
--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
-""" Settings File """
-width=500
-height=200
-targetfps=60
-name="Pyants"
+"""Settings File"""
+
+width = 500
+height = 200
+targetfps = 60
+name = "Pyants"
 WHITE = (255, 255, 255)
 BGBROWN = (42, 21, 3)
+GREEN = (8, 113, 57)
+RED = (184, 13, 20)
+dot_size = 10
+dot_time = 100
+ant_speed = 1
